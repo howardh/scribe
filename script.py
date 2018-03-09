21eadde4e31d3883cf2f18d792ba9eafed78ddaf
Howard HUANG <hhuang63@ds2.CS.McGill.CA>
2018-03-08 22:11:04 -0500

    GPU

--- script.py
+++ script.py
@@ -12,14 +12,20 @@ def load_data():
     return strokes
 
 class GeneratorRNN(torch.nn.Module):
-    def __init__(self, num_components=1):
+    def __init__(self, num_components=1, use_cuda=False):
         super(GeneratorRNN,self).__init__()
+        self.use_cuda = use_cuda
         # See page 23 for parameters
         self.num_components = num_components
         self.lstm = torch.nn.LSTM(input_size=3, hidden_size=900, num_layers=1,
                 bias=True)
         self.linear = torch.nn.Linear(in_features=900,out_features=1+(1+2+2+1)*self.num_components)
         self.softmax = torch.nn.Softmax(dim=0)
+
+        if self.use_cuda:
+            self.lstm = self.lstm.cuda()
+            self.linear = self.linear.cuda()
+            self.softmax = self.softmax.cuda()
 
     def split_outputs(self, output):
         e = output[0]
@@ -55,29 +61,44 @@ class GeneratorRNN(torch.nn.Module):
         return e,pi,mu,sigma,rho,hidden
 
     def init_hidden(self):
-        hidden = Variable(torch.zeros(1, 1, 900))
-        cell = Variable(torch.zeros(1, 1, 900))
+        if self.use_cuda:
+            hidden = Variable(torch.zeros(1, 1, 900).cuda())
+            cell = Variable(torch.zeros(1, 1, 900).cuda())
+        else:
+            hidden = Variable(torch.zeros(1, 1, 900))
+            cell = Variable(torch.zeros(1, 1, 900))
         return [hidden, cell]
 
 def generate_sequence(rnn : GeneratorRNN, length : int):
     """
     Generate a random sequence of handwritten strokes, with `length` strokes.
     """
-    inputs = Variable(torch.zeros(1,3).float())
+    if rnn.use_cuda:
+        inputs = Variable(torch.zeros(1,1,3).float().cuda())
+    else:
+        inputs = Variable(torch.zeros(1,1,3).float())
     hidden = rnn.init_hidden()
     strokes = np.empty([length+1,3])
     strokes[0] = [0,0,0]
     for i in range(1,length+1):
         e,pi,mu,sigma,rho,hidden = rnn.forward(inputs, hidden)
 
-        e = e.data.numpy()
-        rho = rho.data.numpy()
+        if rnn.use_cuda:
+            e = e.data.cpu().numpy()
+            rho = rho.data.cpu().numpy()
+        else:
+            e = e.data.numpy()
+            rho = rho.data.numpy()
 
         # Sample from bivariate Gaussian mixture model
         # Choose a component
         component = np.random.choice(range(rnn.num_components))
-        mu = mu[component].data.numpy()
-        sigma = sigma[component].data.numpy()
+        if rnn.use_cuda:
+            mu = mu[component].data.cpu().numpy()
+            sigma = sigma[component].data.cpu().numpy()
+        else:
+            mu = mu[component].data.numpy()
+            sigma = sigma[component].data.numpy()
 
         # Sample from the selected Gaussian
         covar = [[sigma[0]**2, rho*sigma[0]*sigma[1]],
@@ -119,14 +140,18 @@ def prob(x, y):
     p = 1
     for i in range(num_components):
         p *= pi[i]*normal(x, mu[i],sigma[i],rho)
-        if x[0][0].data.numpy()[0] == 1:
+        if x.is_cuda:
+            temp = x[0][0][0].data.cpu().numpy()[0]
+        else:
+            temp = x[0][0][0].data.numpy()[0]
+        if temp == 1:
             p *= e
         else:
             p *= (1-e)
     return p
 
 def normal(x, mu, sigma, rho):
-    x = x[0][1:]
+    x = x[0][0][1:]
     z = torch.pow((x[0]-mu[0])/sigma[0],2)+torch.pow((x[1]-mu[1])/sigma[1],2)-2*rho*(x[0]-mu[0])*(x[1]-mu[1])/(sigma[0]*sigma[1])
     return 1/(2*np.pi*sigma[0]*sigma[1]*torch.sqrt(1-rho*rho))*torch.exp(-z/(2*(1-rho*rho)))
 
@@ -142,11 +167,14 @@ def train(rnn : GeneratorRNN, strokes):
     # Skipped out on Momentum, since it's not implemented by pytorch
     optimizer = torch.optim.Adam(params=rnn.parameters(),lr=0.0001,betas=(0.95,0.95),eps=0.0001)
     #optimizer = torch.optim.RMSprop(params=rnn.parameters(),lr=0.0001,alpha=0.95,eps=0.0001)
-    strokes_var = Variable(torch.from_numpy(strokes).float(), requires_grad=False)
+    strokes_tensor = torch.from_numpy(strokes).float()
+    if rnn.use_cuda:
+        strokes_tensor = strokes_tensor.cuda()
+    strokes_var = Variable(strokes_tensor, requires_grad=False)
     hidden = rnn.init_hidden()
     loss = 0
     for i in range(len(strokes)):
-        x = strokes_var[i].view(1,-1)
+        x = strokes_var[i].view(1,1,-1)
         e,pi,mu,sigma,rho,hidden = rnn(x, hidden)
         y = (e,pi,mu,sigma,rho)
         loss += -torch.log(prob(x,y))
@@ -155,12 +183,14 @@ def train(rnn : GeneratorRNN, strokes):
     optimizer.step()
 
 def train_all(rnn : GeneratorRNN, data):
+    i = 0
     for strokes in tqdm(data):
+        strokes = generate_sequence(rnn, 100)
+        plot_stroke(strokes, 'output/%d.png'%i)
+        i+=1
         train(rnn, strokes)
 
 if __name__=='__main__':
     data = load_data()
-    rnn = GeneratorRNN(1)
+    rnn = GeneratorRNN(1, use_cuda=True)
     train_all(rnn, data)
-    strokes = generate_sequence(rnn, 100)
-    plot_stroke(strokes, 'output/1.png')
